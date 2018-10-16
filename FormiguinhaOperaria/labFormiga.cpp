9d84cdd291c013de1e0ca055024b503c84bc5a66
Wesley Martins <wesley.martins1900@gmail.com>
2018-10-15 23:05:09 -0300

    CM - Inteligência da formiga
    - Movimentos

--- FormiguinhaOperaria/labFormiga.cpp
+++ FormiguinhaOperaria/labFormiga.cpp
@@ -1,102 +1,142 @@
 #include <iostream>
-#include <fstream> //Para leitura e escrita em arquivo
 #include <windows.h>
-#include <conio.h>
+#include <conio.h> // Para utilizar mgotoxy()
 
 #define L 16
 #define C 34
 
 using namespace std;
 
+// Ocultar o cursor do mouse
 void hidecursor()
 {
-   HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-   CONSOLE_CURSOR_INFO info;
-   info.dwSize = 100;
-   info.bVisible = FALSE;
-   SetConsoleCursorInfo(consoleHandle, &info);
+	// Armazena o estado atual do console
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	
+	// Contém informações sobre o cursor do console
+	CONSOLE_CURSOR_INFO info;
+	info.dwSize = 100;
+	info.bVisible = FALSE;
+
+	//Define o tamanho e a visibilidade do cursor para o buffer de tela do console especificado.
+	SetConsoleCursorInfo(consoleHandle, &info);
 }
 
-void mgotoxy(int x,int y)
+void mgotoxy(int x, int y)
 {
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{x,y});
+	// Move o cursor para determinada posição
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ x, y });
 }
 
 //Nessa função a matriz é percorrida e os números são substituidos
-void imprime(int m[L][C]){
-    for (int i=0;i<L;i++) {
-        for (int j=0;j<C;j++) {
-            if(m[i][j]==1){
-                cout<<(char)178;
-            }
-            else if(m[i][j]==0){
-                cout<<" ";
-            }
-            else if (m[i][j]==9){
-                cout<<"f";
-            } else {
-                cout<<"A";
-            }
+void Imprime(int m[L][C])
+{
+	// A partir dos dados preenchidos na matriz
+	// é criado e exibido o mapa
 
-        }
-        cout<<"\n";
+    for (int i = 0; i < L; i++) 
+	{
+        for (int j = 0; j < C; j++) 
+		{
+			if (m[i][j] == 0)
+			{
+				// Caminho Livre
+				cout << " ";
+			}
+            else if (m[i][j] == 1)
+			{
+				// (char)178 Monta uma "parede" utilizando caractere ASCII
+                cout << (char)178;
+            }
+			else if (m[i][j] == 8)
+			{
+				// Formiga sem carregar alimento
+				cout << "f";
+			} 
+			else if (m[i][j] == 9)
+			{
+				// Formiga carregando alimento
+				cout << "F";
+			}
+			else if (m[i][j] == 5)
+			{
+				// Armazém disponível
+				cout << "A";
+			}
+			else if (m[i][j] == 6)
+			{
+				// Armazém comprometido
+				cout << "C";
+			}
+		}
+		cout << "\n";
     }
 }
 
-
-
-
-void move_formiga(int m[L][C]){
-    static int x=1,y=1;
+void move_formiga(int m[L][C])
+{
+    static int x = 1, y = 1;
     char p;
 
-     if ( _kbhit() ){
-        p = getch();
+	// Aguarda uma tecla ser pressionada
+     if (_kbhit())
+	 {
+		// Captura a última tecla pressionada sem necessidade de pressionar Enter
+		p = getche();
 
-        m[x][y]=0;
+        m[x][y] = 0;
 
+		// Além de identificar se tecla pressionada, é validado se caminho está livre
+		// ou seja, posição igual 0
         switch(p)
         {
-            case 'w': x--; break;      //cima
-            case 's': x++; break;     //baixo
-            case 'a': y--; break;  //esquerda
-            case 'd': y++; break;   //direita
+            case 'w':
+			case 'W':
+				if (x > 0 && m[x--][y] != 0)
+					return;
+				x--;
+				break;      
+            case 's':
+			case 'S':
+				if (x <= L && m[x++][y] != 0)
+					return;
+				x++;
+				break;     
+            case 'a':
+			case 'A':
+				if (y > 0 && m[x][y--] != 0)
+					return;
+				y--; 
+				break;
+            case 'd':
+			case 'D':
+				if (y <= C && m[x][y++] != 0)
+					return;
+				y++;
+				break;
+			default: 
+				return;
         }
-        m[x][y]=9;
-        //Sleep(100);
-        mgotoxy(0,0);
-        imprime(m);
+
+        m[x][y] = 9;
+
+        Sleep(100);
+        mgotoxy(0, 0);
+        Imprime(m);
      }
-
-
 }
 
 int main()
 {
-    int m[L][C]={
-        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-        {1,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
-        {1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1},
-        {1,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,1,1,0,1,1,1,1,1,0,1,0,1,1},
-        {1,0,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1},
-        {1,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1},
-        {1,0,1,1,1,1,1,1,0,1,0,1,1,1,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,1},
-        {1,0,1,0,0,0,0,0,0,1,0,1,0,0,0,1,0,1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,0,1},
-        {1,0,1,0,1,1,1,1,1,1,0,1,0,1,0,1,0,1,0,1,0,1,1,0,1,0,0,0,0,0,0,1,0,1},
-        {1,0,1,0,1,0,0,0,0,0,0,1,0,1,0,1,0,1,1,1,1,1,1,0,1,0,1,1,1,1,0,1,0,1},
-        {1,0,1,0,1,0,1,1,1,1,1,1,0,1,0,1,0,1,1,1,1,0,0,0,1,0,1,1,1,1,0,1,0,1},
-        {1,0,1,0,1,1,1,1,1,1,1,0,0,1,0,1,0,0,1,1,1,0,1,0,1,0,1,0,0,0,0,1,0,1},
-        {1,0,1,0,0,0,0,0,0,0,0,0,1,1,0,1,1,0,1,0,0,0,1,0,0,0,1,0,1,1,1,1,0,1},
-        {1,0,1,1,1,1,1,1,1,1,1,0,1,1,0,0,0,0,1,0,1,1,1,1,1,1,1,0,1,1,1,1,0,1},
-        {1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1},
-        {1,1,1,1,1,1,1,1,1,1,1,3,1,0,1,1,1,1,1,1,1,1,1,1,1,4,1,1,1,1,1,1,1,1},
-    };
+	//// ToDo
+    // m[L][C] = Cenário
+
     hidecursor();
 
-    imprime(m);
+    Imprime(m);
 
-    while(true){
-        move_formiga(m);
-    }
-    return 0;
+	//// ToDo
+	// Motor do Jogo
+	
+	return 0;
 }
